7b3b78c9bdaef3604608338f10001442358071e5
Laszlo Gazsi <laszlo@gazsi.net>
2013-11-09 18:38:38 +0000

    Early development phase

---
+++ lib/rcone.rb
@@ -0,0 +1,66 @@
+module RCone
+  
+  class Cone
+    
+    GET = 'get'
+    SET = 'set'
+    
+    def initialize fields, params, required=[]
+      @rb_field_names = fields
+      @rb_fields = {}
+      
+      fetch_parameters fields, required, params
+    end
+    
+    def method_missing method, *arguments
+      reg_exp = /^(get|set)_(\w*)/
+      match_data = method.to_s.scan reg_exp
+      
+      raise UndefinedMethodException unless match_data.size > 0
+      
+      handle_operation match_data[0][0], match_data[0][1], arguments[0] || nil   
+    end
+    
+    private
+        
+    def handle_operation operation, field, value
+      case operation
+        when GET then get_value field
+        when SET then set_value field, arguments[0]
+      end
+    end
+    
+    def get_value field
+      @rb_fields[field.to_sym]  
+    end
+    
+    def set_value field, value
+      @rb_fields[field.to_sym] = value
+    end
+    
+    def fetch_parameters fields, required, params
+      fields.each do |field|
+        value = params[field]
+        check_required field, value, required
+        @rb_fields[field] = value
+      end
+    end
+    
+    def check_required field, value, required
+      if required.include?(field) && value.nil?
+        raise MissingParameterException.new "Parameter #{param} is undefined!"
+      end
+    end
+    
+  end  
+  
+  class MissingParameterException < Exception
+  end
+  
+  class UndefinedFieldException < Exception
+  end
+  
+  class UndefinedMethodException < Exception
+  end
+  
+end
---
+++ rcone.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'rcone'
+  s.version     = '0.0.1'
+  s.date        = '2013-11-09'
+  s.summary     = "A simple way to handle parameters in Ruby/Sinatra"
+  s.description = "A simple way to handle parameters in Ruby/Sinatra"
+  s.authors     = ["Laszlo Gazsi"]
+  s.email       = 'laszlo@gazsi.net'
+  s.files       = ["lib/rcone.rb"]
+  s.homepage    = 'http://laszlo.gazsi.net'
+  s.license       = 'MIT'
+end
